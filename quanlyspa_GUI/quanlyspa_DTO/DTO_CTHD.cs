73670ddfa824abeca9774f408d0a44ab68041fd0
phamvizuit <90880259+phamvizuit@users.noreply.github.com>
2021-09-19 22:06:56 +0700

    dto cthd,bs
    bruh

---
+++ quanlyspa_GUI/quanlyspa_DTO/DTO_CTHD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quanlyspa_DTO
+{
+    class DTO_HOADON
+    {
+        private string _SOHD;
+        private string _MAMP;
+        private string _THANHTIENMP;
+
+        public string SOHD { get => _SOHD; set => _SOHD = value; }
+        public string MAMP { get => _MAMP; set => _MAMP = value; }
+        public string THANHTIENMP { get => _THANHTIENMP; set => _THANHTIENMP = value; }
+
+    }
+}
