b07334ec723cb73bc666ff935ffe259981f14e22
xuanbach <xuanbachdao0212@gmail.com>
2021-09-19 13:47:36 +0700

    GUI cua Bach

---
+++ quanlyspa_GUI/quanlyspa_DTO/DTO_MYPHAM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quanlyspa_DTO
+{
+    class DTO_MYPHAM
+    {
+        private string _MAMP;
+        private string _TENMP;
+        private string _CHUNGLOAI;
+        private string _HAMLUONG;
+        private string _SOLUONGTONKHO;
+        private string _GIABAN;
+
+
+        public string MAMP { get => _MAMP; set => _MAMP = value; }
+        public string TENMP { get => _TENMP; set => _TENMP = value; }
+        public string CHUNGLOAI { get => _CHUNGLOAI; set => _CHUNGLOAI = value; }
+        public string HAMLUONG { get => _HAMLUONG; set => _HAMLUONG = value; }
+        public string SOLUONGTONKHO { get => _SOLUONGTONKHO; set => _SOLUONGTONKHO = value; }
+        public string GIABAN { get => _GIABAN; set => _GIABAN = value; }
+
+    }
+}
---
+++ quanlyspa_GUI/quanlyspa_DTO/DTO_QLNV.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quanlyspa_DTO
+{
+    class DTO_QLNV
+    {
+        private string _MANV;
+        private string _HOTEN;
+        private string _GIOITINH;
+        private string _NHIEMVU;
+        private string _QUEQUAN;
+        private string _DIACHI;
+        private string _SDT;
+        private string _HESOLUONG;
+        private string _NBDHD;
+        private string _NKTHD;
+
+
+
+        public string MANV { get => _MANV; set => _MANV = value; }
+        public string HOTEN { get => _HOTEN; set => _HOTEN = value; }
+        public string GIOITINH { get => _GIOITINH; set => _GIOITINH = value; }
+        public string NHIEMVU { get => _NHIEMVU; set => _NHIEMVU = value; }
+        public string QUEQUAN { get => _QUEQUAN; set => _QUEQUAN = value; }
+        public string DIACHI { get => _DIACHI; set => _DIACHI = value; }
+        public string SDT { get => _SDT; set => _SDT = value; }
+        public string HESOLUONG { get => _HESOLUONG; set => _HESOLUONG = value; }
+        public string NBDHD { get => _NBDHD; set => _NBDHD = value; }
+        public string NKTHD { get => _NKTHD; set => _NKTHD = value; }
+
+
+    }
+}
