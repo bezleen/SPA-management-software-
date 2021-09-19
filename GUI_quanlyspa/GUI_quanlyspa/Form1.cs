4fa26c7a14b0f787648e662912ed5f884b67bf8b
hienhuynhdang <hienhuynh406@icloud.com>
2021-09-20 00:49:12 +0700

    tao lai project moi

---
+++ GUI_quanlyspa/GUI_quanlyspa/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GUI_quanlyspa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Aqua;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Orange;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bezleen dep trai vai loz!!!");
+        }
+    }
+}
