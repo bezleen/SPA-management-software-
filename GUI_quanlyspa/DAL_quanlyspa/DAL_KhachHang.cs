fd246a953e08113028721b2f6d3316cbdd68c97c
hienhuynhdang <hienhuynh406@icloud.com>
2021-09-21 04:56:06 +0700

    template DAL

---
+++ GUI_quanlyspa/DAL_quanlyspa/DAL_KhachHang.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using quanlyspa_DTO;
+
+namespace DAL_quanlyspa
+{
+    public class DAL_KhachHang
+    {
+        _connectDataBase con = new _connectDataBase();
+
+        // ham insert
+        public void _insertData(DTO_KhachHang val)
+        {
+
+        }
+        // ham update
+        public void _updateData(DTO_KhachHang val)
+        {
+
+        }
+        // ham delete
+        public void _deleteData(DTO_KhachHang val)
+        {
+
+        }
+        // ham select
+        public void _selectData(DTO_KhachHang val)
+        {
+
+        }
+    }
+}
---
+++ GUI_quanlyspa/DAL_quanlyspa/_connectDataBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL_quanlyspa
+{
+    public class _connectDataBase
+    {
+        public static SqlConnection _connect; // default constructor
+
+        public void _KetnoiDB()  // mo ket noi toi database
+        {
+
+        }
+        public void _NgatketnoiDB() // dong ket noi toi database
+        {
+
+        }
+        public void _insert_update_delete(string str) // thuc thi cau lenh them xoa sua bang cach truyen lenh sql vao "str"
+        {
+
+        }
+        public DataTable _select(string str) // thuc thi cau lenh select bang cach truyen lenh sql vao "str" tra ve 1 bang du lieu
+        {
+            DataTable dt=new DataTable();
+            return dt;
+        }
+        public string _value(string str) // thuc thi cau lenh select bang cach truyen lenh sql vao "str" tra ve 1 ket qua . vs lenh count(*) tra ve 5.
+        {
+            string kq = null;
+            return kq;
+        }
+    }
+}
