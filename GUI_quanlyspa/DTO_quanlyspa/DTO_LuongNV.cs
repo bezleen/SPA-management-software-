10f8966ca60b32dcd9151943c0580b188efc3e49
nhandng <19520795@gm.uit.edu.vn>
2021-09-26 22:22:22 +0700

    okay

--- GUI_quanlyspa/DTO_quanlyspa/DTO_LuongNV.cs
+++ GUI_quanlyspa/DTO_quanlyspa/DTO_LuongNV.cs
@@ -9,15 +9,11 @@ namespace quanlyspa_DTO
         private string _MANV;
         private string _MATT;
         private string _NGAYTT;
-        private string _TIENLUONG;
-        private string _TIENTHUONG;
         private string _TONGTIEN;
 
         public string MANV { get => _MANV; set => _MANV = value; }
         public string MATT { get => _MATT; set => _MATT = value; }
         public string NGAYTT { get => _NGAYTT; set => _NGAYTT = value; }
-        public string TIENLUONG { get => _TIENLUONG; set => _TIENLUONG = value; }
-        public string TIENTHUONG { get => _TIENTHUONG; set => _TIENTHUONG = value; }
         public string TONGTIEN { get => _TONGTIEN; set => _TONGTIEN = value; }
     }
 }
