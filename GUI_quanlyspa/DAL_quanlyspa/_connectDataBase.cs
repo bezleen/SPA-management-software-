a8a1ad4fb0b5ce00f282fd2dc80ccfbb8b8599bc
hienhuynhdang <hienhuynh406@icloud.com>
2021-09-26 20:32:38 +0700

    ham them xoa sua

--- GUI_quanlyspa/DAL_quanlyspa/_connectDataBase.cs
+++ GUI_quanlyspa/DAL_quanlyspa/_connectDataBase.cs
@@ -11,17 +11,34 @@ namespace DAL_quanlyspa
     {
         public static SqlConnection _connect; // default constructor
 
-        public void _KetnoiDB()  // mo ket noi toi database
+        public void _KetnoiDB1()  // mo ket noi toi database
         {
-
+            if (_connectDataBase._connect == null)
+                _connectDataBase._connect = new SqlConnection("");
+            if (_connectDataBase._connect.State != ConnectionState.Open)
+                _connectDataBase._connect.Open();
         }
-        public void _NgatketnoiDB() // dong ket noi toi database
+        public void _NgatketnoiDB1() // dong ket noi toi database
         {
-
+            if (_connectDataBase._connect != null && _connectDataBase._connect.State == ConnectionState.Open)
+                _connectDataBase._connect.Close();
         }
         public void _insert_update_delete(string str) // thuc thi cau lenh them xoa sua bang cach truyen lenh sql vao "str"
         {
-
+            try
+            {
+                _KetnoiDB1();
+                SqlCommand _sqlcmd = new SqlCommand(str);
+                _sqlcmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Someting wrong!" + e);
+            }
+            finally
+            {
+                _NgatketnoiDB1();
+            }
         }
         public DataTable _select(string str) // thuc thi cau lenh select bang cach truyen lenh sql vao "str" tra ve 1 bang du lieu
         {
