62f2c02584c265c49ad9db3e32bd589b1a861ccd
phamvizuit <90880259+phamvizuit@users.noreply.github.com>
2021-09-26 22:44:03 +0700

    update dto cthd

--- GUI_quanlyspa/DTO_quanlyspa/DTO_CTHD.cs
+++ GUI_quanlyspa/DTO_quanlyspa/DTO_CTHD.cs
@@ -9,10 +9,12 @@ namespace quanlyspa_DTO
         private string _SOHD;
         private string _MAMP;
         private string _THANHTIENMP;
+        private string _SOLUONG;
 
         public string SOHD { get => _SOHD; set => _SOHD = value; }
         public string MAMP { get => _MAMP; set => _MAMP = value; }
         public string THANHTIENMP { get => _THANHTIENMP; set => _THANHTIENMP = value; }
+        public string SOLUONG { get => _SOLUONG; set => _SOLUONG = value; }
 
     }
 }
