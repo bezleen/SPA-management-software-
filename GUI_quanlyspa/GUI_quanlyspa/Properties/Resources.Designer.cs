a8a850c8d7d777161b66f9d3f8db516790537b9e
hienhuynhdang <hienhuynh406@icloud.com>
2021-10-18 17:32:33 +0700

    giao dien home + lieutrinh

--- GUI_quanlyspa/GUI_quanlyspa/Properties/Resources.Designer.cs
+++ GUI_quanlyspa/GUI_quanlyspa/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace GUI_quanlyspa.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap background {
+            get {
+                object obj = ResourceManager.GetObject("background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap delete {
             get {
                 object obj = ResourceManager.GetObject("delete", resourceCulture);
