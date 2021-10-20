83dc72a05a009fe07dfadba624b25932cd6fea94
xuanbach <xuanbachdao0212@gmail.com>
2021-10-20 15:03:41 +0700

    bus

---
+++ GUI_quanlyspa/BUS_quanlyspa/BUS_QLNCC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using quanlyspa_DTO;
+using DAL_quanlyspa;
+
+namespace BUS_quanlyspa
+{
+    public class BUS_QLNCC
+    {
+        DAL_QLYNCC sql = new DAL_QLYNCC();
+        // ham insert
+        public void _insertData(DAL_QLYNCC val)
+        {
+            sql._insertData(val);
+        }
+        // ham update
+        public void _updateData(DAL_QLYNCC val)
+        {
+            sql._updateData(val);
+        }
+        // ham delete
+        public void _deleteData(DAL_QLYNCC val)
+        {
+            sql._deleteData(val);
+        }
+        // ham selectS
+        public DataTable _selectData(string str)
+        {
+            return sql._selectData(str);
+        }
+    }
+}
---
+++ GUI_quanlyspa/BUS_quanlyspa/BUS_QLNV.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using quanlyspa_DTO;
+using DAL_quanlyspa;
+
+
+namespace BUS_quanlyspa
+{
+    public class BUS_QLNV
+    {
+        DAL_QLNV sql = new DAL_QLNV();
+        // ham insert
+        public void _insertData(DAL_QLNV val)
+        {
+            sql._insertData(val);
+        }
+        // ham update
+        public void _updateData(DAL_QLNV val)
+        {
+            sql._updateData(val);
+        }
+        // ham delete
+        public void _deleteData(DAL_QLNV val)
+        {
+            sql._deleteData(val);
+        }
+        // ham selectS
+        public DataTable _selectData(string str)
+        {
+            return sql._selectData(str);
+        }
+    }
+}
